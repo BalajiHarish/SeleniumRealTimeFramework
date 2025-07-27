8422d366e5c980b76bd1ed063d97c3435da55f3b
BalajiHarish <balajiharish21@gmail.com>
2025-07-27 14:48:42 +0530

    Comments for DriverFactory.java

--- src/main/java/com/saucedemo/driver/DriverFactory.java
+++ src/main/java/com/saucedemo/driver/DriverFactory.java
@@ -12,10 +12,44 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * {@code DriverFactory} is a utility class responsible for creating and returning
+ * {@link WebDriver} instances based on the specified browser and execution mode.
+ * <p>
+ * It supports both local and remote (Selenium Grid) execution.
+ * The execution mode is determined by the <b>RUNMODE</b> property
+ * configured in the project's property file.
+ * </p>
+ *
+ * <p><b>Supported browsers:</b></p>
+ * <ul>
+ *     <li>Chrome</li>
+ *     <li>Edge</li>
+ *     <li>Firefox</li>
+ * </ul>
+ *
+ * <p><b>Example usage:</b></p>
+ * <pre>{@code
+ * WebDriver driver = DriverFactory.getDriver("Chrome");
+ * }</pre>
+ *
+ * <p>This class cannot be instantiated.</p>
+ *
+ * @author Balaji
+ */
+
 public final class DriverFactory {
 
     private DriverFactory() {
     }
+
+    /**
+     * Returns a {@link WebDriver} instance based on the specified browser name and execution mode.
+     *
+     * @param browser the name of the browser ("Chrome", "Edge", or "Firefox")
+     * @return a configured {@link WebDriver} instance
+     * @throws MalformedURLException if the Selenium Grid URL is malformed when using remote execution
+     */
 
     public static WebDriver getDriver(String browser) throws MalformedURLException {
 
